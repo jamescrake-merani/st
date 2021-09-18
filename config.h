f17c5f192b59ef19390ff158406abe438214c9ad
James Crake-Merani <james@trutho.net>
2021-09-18 15:54:43 +0100

    Patched for emoticon font2

--- config.h
+++ config.h
@@ -6,6 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Jetbrains Mono:pixelsize=30:antialias=true:autohint=true";
+static char *font2[] = {"JoyPixels:pixelsize=10:antialias=true:autohint=true"};
 static int borderpx = 2;
 
 // Opacity
